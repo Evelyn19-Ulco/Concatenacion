30e3d5262aa0992deeaff8d94e1a0ba714d7abda
Evelyn19-Ulco <76453340+Evelyn19-Ulco@users.noreply.github.com>
2021-02-05 11:37:53 -0500

    Add files via upload

---
+++ concatenacion.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 14:46:20 2021
+
+@author: DELL
+"""
+
+str1='Cisco'
+str2='Networking'
+str3='Academy'
+space=" "
+print(str1+space+str2+space+str3)
+print('\n') # Saltos de linea
+print(str1+str2+str3,'\n' *2)
+print(str1,str2,str3,'\n' *2)
+x=5
+print(type(x),'\n')
+print('El valor de x es: '+str(x))
+print(type(x))
+x=str(x) # string
+print(type(x))
+x=int(x) #entero
+print(type(x))
+x=float(x)
+print(type(x))
+x=bool(x)
+print(type(x))
